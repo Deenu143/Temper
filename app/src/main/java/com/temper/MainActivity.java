94409fea12d42a082a18b980a701c80638e8abb6
deenu143 <86369518+Deenu143@users.noreply.github.com>
2023-03-07 00:27:59 +0530

    Update MainActivity.java

--- app/src/main/java/com/temper/MainActivity.java
+++ app/src/main/java/com/temper/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
     }
 	static {
 		System.loadLibrary("app-integrity-checker");
